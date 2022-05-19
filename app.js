e57a5430d61d042c484ca18bac2ac5c82d33a291
Salman <Salmanrajputsr71@gmail.com>
2022-05-19 15:42:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,169 @@
+                                             ///  STRING METHODS  ///
+
+ //  Question 01 Start  //
+
+// var a = prompt(" Enter your first Name ?");
+// var b = prompt(" Enter your Last Name ?");
+// document.write(a,b);
+
+//  Question 01 End  //
+
+//  Question 02 Start  //
+
+// var a = prompt(" What is your favorite Mobile Phone ?");
+// console.log(a)
+// document.write(a.length);
+
+//  Question 02 End  //
+
+//  Question 03 Start  //
+
+//  var names = "Pakistani";
+// var name1 = names.indexOf("n");
+// document.write("Index of 'n'" + " "+ name1);
+
+//  Question 03 End  //
+
+//  Question 04 Start  //
+
+// var names = "Hell World";
+// var name1 = names.lastIndexOf("l");
+// document.write(" Last index of 'l'" + " "+ name1);
+
+//  Question 04 End  //
+
+//  Question 05 Start  //
+
+// var names = "Pakistani";
+// var name1 = names.charAt(3);
+// document.write("Character at  index '3'" + " "+ name1);
+
+// Question 05 End  //
+
+//  Question 06 Start  //
+
+// var firstName = prompt("Enter Your FirstName");
+// var lastName = prompt("Enter Your LastName");
+// var full = firstName.concat(lastName);
+// document.write(full);
+
+//  Question 06 End  //
+
+//  Question 07 Start  //
+
+// var city = "Hyderbad";
+// var rep = city.replace("Hyder","Islama");
+// document.write(rep);
+
+//  Question 07 End  //
+
+//  Question 08 Start  //
+
+// var mesg = "Ali and Sami are best friends. They play cricket and football together.";
+// var rep = mesg.replace(/and/g ,"&");
+// document.write(rep);
+
+//  Question 08 End  //
+
+//  Question 09 Start  //
+
+// var type = "472";
+// var huh = Number(type);
+// document.write(huh);
+
+//  Question 09 End  //
+
+//  Question 10 Start  //
+
+// var names = prompt("Enter Your Name").toUpperCase();
+// document.write(names)
+
+//  Question 10 End  //
+
+// Question 11 Start  //
+
+// var userFirstName =prompt("Enter Your Name");
+
+// var firsatname = userFirstName[0].toUpperCase();
+// var othername = userFirstName.slice(1).toLowerCase();
+// document.write(firsatname+othername);
+
+//  Question 11 End  //
+
+//  Question 12 Start  //
+
+// var num = 35.36;
+// var names =num.toString().replace(".", ""); 
+// document.write(names)
+
+//  Question 12 End  //
+
+//  Question 13 Start  //
+
+// var username = prompt("Enter special name");
+// var arr =["@","!",",",".","$"];
+// for(var a =0; a=arr.length)
+
+//  Question 13 End  //
+
+//  Question 14 Start  //
+
+// var user = prompt("enter item")
+// var isMatch= "no"
+// var items = ["cokies " ,"mutton " , "juice" , "biscuit"]
+//  for (var i = 0 ; i<items.length ; i++){
+//      if(user === items[i]){
+//          alert(user + " is available at index " + i +" in our bakery ")
+//     isMatch= "yes"
+//         }
+//  }if (isMatch == "no"){
+//      alert("not available")
+//  }
+
+//  Question 14 End  //
+
+//  Question 15 Start  //
+
+// var password = prompt("Enter Password!");
+// var pass_validation = /^[a-zA-Z0-9!$@]{1,}$/
+// if (password !== "") {
+//     if (!password.match(pass_validation)) {
+//         alert("Please enter a valid Password")
+//     }
+//     else {
+//         alert("Password is Correct")
+//     }
+
+// }
+// else {
+//     alert("Please fill up password field")
+// }
+
+//  Question 15 End  //
+
+//  Question 16 Start  //
+
+// var university = "University of Islamabad";
+// var arr = university.split("")
+// document.write(arr.join("<br />"))
+
+//  Question 16 End  //
+
+//  Question 17 Start  //
+
+// var names = "Pakistan";
+// var name1=names.slice(-1);
+// document.write(name1);
+
+//  Question 17 End  //
+
+//  Question 18 Start  //
+
+// var string = "The quick brown fox jumps over the lazy dog".toLowerCase();
+// var count = string.match(/the/g)
+// console.log(count.length)
+
+//  Question 18 End  //
+ 
+                           
+                
